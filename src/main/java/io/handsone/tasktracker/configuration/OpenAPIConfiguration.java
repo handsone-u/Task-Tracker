2fa220ab69b2ff2edce26bf4a28c00ed2a44be30
handsone-u <handsoneu@gmail.com>
2025-02-26 17:22:59 +0900

    add swagger-ui

---
+++ src/main/java/io/handsone/tasktracker/configuration/OpenAPIConfiguration.java
@@ -0,0 +1,28 @@
+package io.handsone.tasktracker.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+    info = @Info(title = "Task-Tracker API", version = "0.0.1", description = "Specs of Task-Tracker service."),
+    servers = {
+        @Server(url = "http://localhost:8080", description = "Local"),
+//        @Server(url = "https://handsone.o-r.kr", description = "Dev"),
+    },
+    security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+    type = SecuritySchemeType.HTTP,
+    name = "bearerAuth",
+    in = SecuritySchemeIn.HEADER,
+    bearerFormat = "JWT",
+    scheme = "bearer"
+)
+public class OpenAPIConfiguration {
+
+}
